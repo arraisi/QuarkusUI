644f9a7e4d519ad58db1f6bce5fc608582c021b3
Abdul R. Arraisi <arraisi@Abduls-MacBook-Pro.local>
2022-01-12 13:12:56 +0700

    add person datatables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,27 @@
 <template>
 	<v-container fluid>
 		<v-col>
-			<h1 style="text-align: center">Datatable with 3rd Party API</h1>
+			<h1 style="text-align: center">Person</h1>
+			<v-data-table
+				:footer-props="{
+					'items-per-page-options': [2, 4, 8]
+				}"
+				:items-per-page="2"
+				:headers="headers"
+				:items="data"
+				:options.sync="options"
+				:server-items-length="serverItemsLength"
+				:loading="loading"
+				:sort-by.sync="sortBy"
+				:sort-desc.sync="sortDesc"
+			>
+				<template v-slot:item.logo="{ item }">
+					<img :src="item.airline.logo" style="width: 10%" />
+				</template>
+				<template v-slot:item.website="{ item }">
+					<a :href="item.airline.website">{{ item.airline.website }}</a>
+				</template>
+			</v-data-table>
 		</v-col>
 	</v-container>
 </template>
@@ -13,24 +33,57 @@ export default {
 	name: "HelloWorld",
 	data: () => ({
 		loading: true,
-		data: []
+		data: [],
+		options: {},
+		headers: [
+			{ text: "ID", value: "id" },
+			{ text: "Name", value: "name" },
+			{ text: "Email", value: "email" },
+			{ text: "Active", value: "active" }
+		],
+		sortBy: "id",
+		sortDesc: false,
+		serverItemsLength: 0
 	}),
+	//this one will populate new data set when user changes current page.
+	watch: {
+		options: {
+			handler() {
+				this.personDatatables();
+			}
+		},
+		deep: true
+	},
 	methods: {
 		//Reading data from API method.
-		readDataFromAPI() {
+		personList() {
 			this.loading = true;
 			axios.get("/person/list").then((response) => {
 				//Then injecting the result to datatable parameters.
 				this.loading = false;
-        console.log('response: ', response);
-        console.log("response data: ", response.data);
+				// console.log("response: ", response);
+				// console.log("response data: ", response.data);
 				this.data = response.data.data;
+			});
+		},
+		personDatatables() {
+			this.loading = true;
+			const { page, itemsPerPage } = this.options;
+			let pageIndex = page - 1;
+			axios.get(`/person/datatables?pageIndex=${pageIndex}&pageSize=${itemsPerPage}`).then((response) => {
+				//Then injecting the result to datatable parameters.
+				this.loading = false;
+				console.log("response: ", response);
+				this.data = response.data.item1;
+				this.serverItemsLength = response.data.item2;
+				// console.log("data: ", this.data);
 			});
 		}
 	},
 	//this will trigger in the onReady State
 	mounted() {
-		this.readDataFromAPI();
+		this.personList();
+		this.personDatatables();
 	}
 };
 </script>
